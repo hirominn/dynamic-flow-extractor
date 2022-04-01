361932cb3436f9d5049178737ba99d326717cdf6
hirominn <oshimihiroto@gmail.com>
2022-04-02 01:12:16 +0900

    connect to HoloLens

--- extract.py
+++ extract.py
@@ -1,10 +1,12 @@
 import os
+from shutil import move
 import numpy as np
 import cv2 as cv
 import colorsys
 import glob
 import sys
 import time
+import socket
 
 from util import mmap_manager
 from yolact import gen_mask
@@ -16,7 +18,7 @@ def calc_iou(flow, masks, bboxes, classes):
     # end if the image has no hand
     if next((f for f in classes if f == 0), None) == None:
         print('no hands')
-        return
+        return ''
 
     ### Calclate environment flow ###
     for j in range(len(masks)):
@@ -30,7 +32,7 @@ def calc_iou(flow, masks, bboxes, classes):
         obj = cv.bitwise_not(obj)
         # cv.imwrite('masks2/mask_{}.png'.format(j), obj)
         env_flow = cv.bitwise_and(env_flow, env_flow, mask=obj)
-        # cv.imwrite('result/env_flow.png', env_flow)
+    cv.imwrite('result/env_flow.png', env_flow)
 
     # print(env_flow.shape)
     ave_bgr = np.average(env_flow[np.where(env_flow[:, :, 0] + env_flow[:, :, 1] + env_flow[:, :, 2] != 0)], axis=0)
@@ -48,29 +50,40 @@ def calc_iou(flow, masks, bboxes, classes):
     # cv.imwrite('result/flow_clip.png', flow_clip)
     # cv.imwrite('result/flow_rec_diff.png', flow_rec_diff)
 
-    with open('result/result_iou.txt', mode='w') as f:
-        for j in range(len(masks)):
-            black = (masks[j]*255).astype(np.uint8)
-            # cv.imwrite('masks/mask_{}.png'.format(j), black)
-            mask = black
-            bbox = np.array([(bboxes[j][0], bboxes[j][1]), (bboxes[j][2], bboxes[j][1]), (bboxes[j][2], bboxes[j][3]), (bboxes[j][0], bboxes[j][3])])
-            stencil = np.zeros(flow_rec_diff.shape).astype(flow_rec_diff.dtype)
-            cv.fillPoly(stencil, [bbox], [255, 255, 255])
-            result = cv.bitwise_and(flow_rec_diff, stencil)
-            # cv.imwrite('masks/bboxes_{}.png'.format(j), result)
-            intersection = np.logical_and(result, mask)
-            union = np.logical_or(result, mask)
-            iou_score = np.sum(intersection) / np.sum(union)
-            f.write('{}, class: {}, iou: {}, bbox:{}/{}/{}/{}\n'.format(j, classes[j], iou_score, bboxes[j][0], bboxes[j][1], bboxes[j][2], bboxes[j][3]))
-            if iou_score >= 0.2:
-                print('{}, class: {}, iou: {}, bbox:{}/{}/{}/{}'.format(j, classes[j], iou_score, bboxes[j][0], bboxes[j][1], bboxes[j][2], bboxes[j][3]))
+    moved_object_image = np.zeros((384, 640), dtype=np.uint8)
+
+    items = ''
+    for j in range(len(masks)):
+        black = (masks[j]*255).astype(np.uint8)
+        # cv.imwrite('masks/mask_{}.png'.format(j), black)
+        mask = black
+        bbox = np.array([(bboxes[j][0], bboxes[j][1]), (bboxes[j][2], bboxes[j][1]), (bboxes[j][2], bboxes[j][3]), (bboxes[j][0], bboxes[j][3])])
+        stencil = np.zeros(flow_rec_diff.shape).astype(flow_rec_diff.dtype)
+        cv.fillPoly(stencil, [bbox], [255, 255, 255])
+        result = cv.bitwise_and(flow_rec_diff, stencil)
+        # cv.imwrite('masks/bboxes_{}.png'.format(j), result)
+        intersection = np.logical_and(result, mask)
+        union = np.logical_or(result, mask)
+        iou_score = np.sum(intersection) / np.sum(union)
+        items += 'class:{},bbox:{}/{}/{}/{},iou:{}\n'.format(classes[j], iou_score, bboxes[j][0]/640, bboxes[j][1]/384, bboxes[j][2]/640, bboxes[j][3]/384)
+        if(iou_score > 0.2):
+            moved_object_image = cv.bitwise_or(moved_object_image, mask)
+    cv.imwrite('result/moved_object_image.png', moved_object_image)
+    return items
 
 if __name__ == '__main__':
     os.makedirs('masks', exist_ok=True)
     os.makedirs('result', exist_ok=True)
 
-    trained_model='yolact/weights/yolact_plus_resnet50_54_800000.pth'
-    net = gen_mask.prepare_net(trained_model)
+    port = 4000
+    serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serversock.bind(('',port))
+    serversock.listen(10)
+    print('Waiting for connections...')
+    clientsock, client_address = serversock.accept()
+    print("Succeeded in Connection!")
+    print(client_address)
 
     shape = (384, 640, 3)
     img_size = np.prod(shape)
@@ -78,37 +91,62 @@ if __name__ == '__main__':
     mm_image_out = mmap_manager.mmapManager('./flow_img_out.dat', img_size)
     mm_status = mmap_manager.mmapManager('./status.dat', 4)
 
+    trained_model='yolact/weights/yolact_plus_resnet50_54_800000.pth'
+    net = gen_mask.prepare_net(trained_model)
+
     cnt = 0
 
     images = glob.glob(os.path.join(sys.path[0], 'demo-frames', '*.png')) + \
             glob.glob(os.path.join(sys.path[0], 'demo-frames', '*.jpg'))
     images = sorted(images)
-    for imfile in images:
-    # while cnt < 10:
-        cnt += 1
-        print(cnt)
+    # for imfile in images:
+    try:
+        while True:
+            cnt += 1
+            print(cnt)
 
-        cam_image = cv.imread(imfile)
+            # cam_image = cv.imread(imfile)
+            buffer_size = int.from_bytes(clientsock.recv(8), 'little') # int64 / 8 = 8
+            print('size :', buffer_size)
+            clientsock.send(bytes('received', 'utf-8'))
+            print('sent ack')
+            data = b''
+            while len(data) < buffer_size :
+                diffdata = clientsock.recv(buffer_size)
+                data += diffdata
+            print('received image data')
+            tmp = np.frombuffer(data, np.uint8, -1)
+            img = cv.imdecode(tmp, cv.IMREAD_COLOR)
+            cam_image = cv.resize(img, dsize=(640, 384))
 
-        mm_image_in.WriteImage(cam_image)
-        if cnt == 1:
-            print('first')
-            mm_status.WriteString('frst')
-        else:
-            print('sent')
-            mm_status.WriteString('sent')
+            mm_image_in.WriteImage(cam_image)
+            if cnt == 1:
+                print('first')
+                mm_status.WriteString('frst')
+            else:
+                print('sent')
+                mm_status.WriteString('sent')
 
-        # generate mask
-        masks, bboxes, classes = gen_mask.gen_mask(net, cam_image)
-        while mm_status.ReadString() in ['sent', 'frst']:
-            pass
-        flow = mm_image_out.ReadImage()
+            # generate mask
+            masks, bboxes, classes = gen_mask.gen_mask(net, cam_image)
+            while mm_status.ReadString() in ['sent', 'frst']:
+                pass
+            flow = mm_image_out.ReadImage()
 
-        if cnt > 1:
-            start_iou = time.perf_counter()
-            calc_iou(flow, masks, bboxes, classes)
-            stop_iou = time.perf_counter()
-            print("Calculate IoU:", (stop_iou - start_iou) * 1000, "ms")
-    mm_image_in.dispose()
-    mm_image_out.dispose()
-    mm_status.dispose()
+            if cnt > 1:
+                start_iou = time.perf_counter()
+                items = calc_iou(flow, masks, bboxes, classes)
+                stop_iou = time.perf_counter()
+                # print("Calculate IoU:", (stop_iou - start_iou) * 1000, "ms")
+
+                # with open("runs/detect/exp/labels/out.txt", "r") as file:
+                #     inferred_labels = file.read()
+                clientsock.send(bytes('person 0 0.253125 0.559896 0.240625 0.4375 0.679568', 'utf-8'))
+                print(items)
+            else: 
+                print('')
+                clientsock.send(bytes('person 0 0.253125 0.559896 0.240625 0.4375 0.679568', 'utf-8'))
+    except KeyboardInterrupt:
+        mm_image_in.dispose()
+        mm_image_out.dispose()
+        mm_status.dispose()
--- flow_generator.py
+++ flow_generator.py
@@ -8,53 +8,54 @@ from util import mmap_manager
 from RAFT import gen_flow
 
 if __name__ == '__main__':
-    shape = (384, 640, 3)
-    img_size = np.prod(shape)
-    mm_image_in = mmap_manager.mmapManager('./flow_img_in.dat', img_size)
-    mm_image_out = mmap_manager.mmapManager('./flow_img_out.dat', img_size)
-    mm_status = mmap_manager.mmapManager('./status.dat', 4)
+    try:
+        shape = (384, 640, 3)
+        img_size = np.prod(shape)
+        mm_image_in = mmap_manager.mmapManager('./flow_img_in.dat', img_size)
+        mm_image_out = mmap_manager.mmapManager('./flow_img_out.dat', img_size)
+        mm_status = mmap_manager.mmapManager('./status.dat', 4)
 
-    raft_args = argparse.Namespace(model='RAFT/models/raft-things.pth', path='RAFT/demo-frames', small=False, mixed_precision=True, alternate_corr=False)
-    raft_model = gen_flow.load_model(raft_args)
+        raft_args = argparse.Namespace(model='RAFT/models/raft-things.pth', path='RAFT/demo-frames', small=False, mixed_precision=True, alternate_corr=False)
+        raft_model = gen_flow.load_model(raft_args)
 
-    cnt = 0
-    while True:
-    # while cnt < 10:
-        start = time.perf_counter()
+        cnt = 0
+        while True:
+            start = time.perf_counter()
 
-        start_wait = time.perf_counter()
+            start_wait = time.perf_counter()
 
-        while mm_status.ReadString() == 'rply':
-            continue
-        cnt += 1
+            while mm_status.ReadString() == 'rply':
+                continue
+            cnt += 1
 
-        stop_wait = time.perf_counter()
-        print("Wait:", (stop_wait - start_wait) * 1000, "ms")
+            stop_wait = time.perf_counter()
+            print("Wait:", (stop_wait - start_wait) * 1000, "ms")
 
-        # image1 = gen_flow.load_image_from_file('demo-frames/out_0067.jpg')
-        # image2 = gen_flow.load_image_from_file('demo-frames/out_0068.jpg')
+            # image1 = gen_flow.load_image_from_file('demo-frames/out_0067.jpg')
+            # image2 = gen_flow.load_image_from_file('demo-frames/out_0068.jpg')
 
-        status = mm_status.ReadString()
-        if status == 'frst':
-            print('hi')
-            image_now = gen_flow.load_image_from_cv(mm_image_in.ReadImage())
+            status = mm_status.ReadString()
+            if status == 'frst':
+                print('hi')
+                image_now = gen_flow.load_image_from_cv(mm_image_in.ReadImage())
+                mm_status.WriteString('rply')
+                continue
+            else:
+                print('HI')
+                image_pre = image_now
+                start_load = time.perf_counter()
+                image_now = gen_flow.load_image_from_cv(mm_image_in.ReadImage())
+                stop_load = time.perf_counter()
+                print("Loading Image:", (stop_load - start_load) * 1000, "ms")
+                mm_image_out.WriteImage(gen_flow.demo(raft_args, raft_model, image_now, image_pre))
+
+            # cv.imwrite("result.png", img)
+
             mm_status.WriteString('rply')
-            continue
-        else:
-            print('HI')
-            image_pre = image_now
-            start_load = time.perf_counter()
-            image_now = gen_flow.load_image_from_cv(mm_image_in.ReadImage())
-            stop_load = time.perf_counter()
-            print("Loading Image:", (stop_load - start_load) * 1000, "ms")
-            mm_image_out.WriteImage(gen_flow.demo(raft_args, raft_model, image_now, image_pre))
 
-        # cv.imwrite("result.png", img)
-
-        mm_status.WriteString('rply')
-
-        stop = time.perf_counter()
-        print("Flow Generation:", (stop - start) * 1000, "ms")
-    mm_image_in.dispose()
-    mm_image_out.dispose()
-    mm_status.dispose()
+            stop = time.perf_counter()
+            print("Flow Generation:", (stop - start) * 1000, "ms")
+    except KeyboardInterrupt:
+        mm_image_in.dispose()
+        mm_image_out.dispose()
+        mm_status.dispose()
